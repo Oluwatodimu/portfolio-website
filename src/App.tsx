4a38c5cc317497440b8324d17bcb5e8a2509049d
todimuisewon <oluwatodimu.isewon@payu.com>
2025-09-21 23:21:10 +0100

    change website name

--- src/App.tsx
+++ src/App.tsx
@@ -9,18 +9,18 @@ import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 
 function App() {
-  return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/work" element={<Work />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router basename="/portfolio-website">
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/work" element={<Work />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
